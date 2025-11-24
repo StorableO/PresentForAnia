6912a3efdf7eafbb8e7744b5e8b583fdb6703b5b
StorableO <dikundenis19@gmail.com>
2025-11-24 15:57:24 +0100

    Adding main page template

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import TextSequenceAnimation from './components/startWindowAnim/TextSequenceAnimation';
+import PageContent from './components/mainWindow/pageContent';
 
 const App = () => {
   return (
     <div className="App">
-      <TextSequenceAnimation></TextSequenceAnimation>
+      <PageContent></PageContent>
     </div>
   );
 };
