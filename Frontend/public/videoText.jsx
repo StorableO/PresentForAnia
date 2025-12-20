b35dec256e7c7b8a011dd8222e8bb10832f41b86
Denis <166916866+StorableO@users.noreply.github.com>
2025-12-20 09:14:56 +0100

    Update videoText.jsx

--- Frontend/public/videoText.jsx
+++ Frontend/public/videoText.jsx
@@ -1,51 +1,51 @@
-import n1 from '../src/assets/videos/n1.mp4';
-import n2 from '../src/assets/videos/n2.mp4';
-import n3 from '../src/assets/videos/n3.mp4';
-import n4 from '../src/assets/videos/n4.mp4';
-import n5 from '../src/assets/videos/n5.mp4';
-import n6 from '../src/assets/videos/n6.mp4';
+// import n1 from '../src/assets/videos/n1.mp4';
+// import n2 from '../src/assets/videos/n2.mp4';
+// import n3 from '../src/assets/videos/n3.mp4';
+// import n4 from '../src/assets/videos/n4.mp4';
+// import n5 from '../src/assets/videos/n5.mp4';
+// import n6 from '../src/assets/videos/n6.mp4';
 
  export const videoMoments = [
     {
       id: 1,
       title: "Выход из дома",
       description: "Тот самый день, когда все началось.",
-      url: n1,
+      url: "",
       color: "#ffccd5"
     },
     {
       id: 2,
       title: "Опоздание на автобус",
       description: "Задумки не бывают без проблем :)",
-      url: n2,
+      url: "",
       color: "#c9e4ff"
     },
     {
       id: 3,
       title: "Надо покушать",
       description: "Каждая поездка с тобой — это приключение.",
-      url: n3,
+      url: "",
       color: "#d0f0c0"
     },
     {
       id: 4,
       title: "Музей",
       description: "Одно из самых знаменитых музеев тут",
-      url: n4,
+      url: "",
       color: "#fcf4a3"
     },
     {
       id: 5,
       title: "Покупка шариков",
       description: "Выбор между этих двух вариантов был трудным",
-      url: n5,
+      url: "",
       color: "#e6d3ff"
     },
     {
       id: 6,
       title: "Едем домой",
       description: "Все еще впереди...",
-      url: n6,
+      url: "",
       color: "#ffd8b1"
     }
   ];
