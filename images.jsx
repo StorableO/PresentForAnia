bb4d7a805fe672abd44ce7f5c7eca11557dd4a71
StorableO <166916866+StorableO@users.noreply.github.com>
2026-02-04 15:58:26 +0000

    Deploying to gh-pages from @ StorableO/PresentForAnia@0744bde379b2ffda352b9e17c35885e2765a9f04 🚀

--- images.jsx
+++
@@ -1,8 +0,0 @@
-export const imageData = [
-    { id: 1, url: '', caption: 'Наше первое фото' },
-    { id: 2, url: '', caption: 'Закат вместе' },
-    { id: 3, url: '', caption: 'Твой прекрасный взгляд' },
-    { id: 4, url: '', caption: 'Улыбка, которая согревает' },
-    { id: 5, url: '', caption: 'Наше путешествие' },
-    { id: 6, url: '', caption: 'Прогулка под дождем' },
-  ];
